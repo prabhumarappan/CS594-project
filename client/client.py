1885ee9c0f98b63f7c3c1b9fe208e9a0c4ca3b0e
Prabhu <prabhum.794@gmail.com>
2022-12-08 12:18:51 +0530

    modified code as per pylint warnings

--- client/client.py
+++ client/client.py
@@ -1,6 +1,5 @@
 import sys
 import socket
-import sys
 import os
 import json
 import select
@@ -10,33 +9,33 @@ SERVER_PORT = int(os.getenv("SERVER_PORT", 1078))
 MSS = 1024
 
 
-"""
-IRC Client to interact with user through CLI and communicate the same with the server
-"""
-
-
 class IRCClient:
+    """
+    IRC Client to interact with user through CLI and communicate the same with the server
+    """
+
     def __init__(self, client_name) -> None:
         """
         Start a new client, connect them to the server by sending a intilaization message.
 
         Args:
-                client_name (str): Name of the client that will be send to the for future correspondence
+                client_name (str): Name of the client that will be send to the for future
+                correspondence
         """
-        print("Connecting Client %s To Server" % client_name, flush=True)
+        print(f"Connecting Client {client_name} To Server", flush=True)
         self.client_name = client_name
-        self.clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clientSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.clientSock.connect((SERVER_ADDRESS, SERVER_PORT))
+        self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.client_sock.connect((SERVER_ADDRESS, SERVER_PORT))
         data = {
             "command": "CLIENTINIT",
             "room_name": client_name,
             "clientname": client_name,
-            "message": "Joining %s Server" % client_name,
+            "message": f"Joining {client_name} Server",
         }
-        dataToSend = json.dumps(data).encode("utf-8")
-        self.clientSock.sendall(dataToSend)
-        print("Connected Client %s To Server" % client_name, flush=True)
+        data_to_send = json.dumps(data).encode("utf-8")
+        self.client_sock.sendall(data_to_send)
+        print(f"Connected Client {client_name} To Server", flush=True)
 
     def make_payload(
         self, command, client_name, message=None, room_name=None, receiver=None
@@ -48,14 +47,19 @@ class IRCClient:
                 command (str): Command that needs to be executed at the server
                 client_name (str): Client name that is sending the message
                 message (str): The message that needs to be sent along. Defaults to None.
-                room_name (str, optional): Room name that message needs to be sent to. Defaults to None.
-                receiver (str, optional): Name of the receiver that will receive the message. Defaults to None.
-
+                room_name (str, optional): Room name that message needs to be sent to.
+                Defaults to None.
+                receiver (str, optional): Name of the receiver that will receive the message.
+                Defaults to None.
         Returns:
                 dict: Paylaod for the server
         """
 
-        data = {"clientname": client_name, "message": message, "command": command}
+        data = {
+            "clientname": client_name,
+            "message": message,
+            "command": command,
+        }
 
         if room_name:
             data["room_name"] = room_name
@@ -64,9 +68,16 @@ class IRCClient:
 
         return data
 
-    def make_and_send_payload(
-        self, command, message=None, room_name=None, receiver=None
-    ):
+    def make_and_send_payload(self, command, message=None, room_name=None, receiver=None):
+        """
+        Function to make the payload that should be sent to the server and send it to the server
+
+        Args:
+            command (_type_): _description_
+            message (_type_, optional): _description_. Defaults to None.
+            room_name (_type_, optional): _description_. Defaults to None.
+            receiver (_type_, optional): _description_. Defaults to None.
+        """
         data = self.make_payload(
             command=command,
             client_name=self.client_name,
@@ -74,12 +85,13 @@ class IRCClient:
             room_name=room_name,
             receiver=receiver,
         )
-        dataToSend = json.dumps(data).encode("utf-8")
-        self.clientSock.sendall(dataToSend)
+        data_to_send = json.dumps(data).encode("utf-8")
+        self.client_sock.sendall(data_to_send)
 
     def create_chat_room(self, name):
         """
-        Function to create a chat room from the clients end. Client will format and send a message to server so that it can create the specific chat room
+        Function to create a chat room from the clients end. Client will format and send a
+        message to server so that it can create the specific chat room
 
         Args:
                 name (str): Name of the chat room that needs to be created
@@ -87,7 +99,7 @@ class IRCClient:
         self.make_and_send_payload(
             command="CREATECHATROOM",
             room_name=name,
-            message="%s created this chatroom" % (self.client_name),
+            message=f"{self.client_name} created this chatroom",
         )
 
     def join_chat_room(self, name):
@@ -100,7 +112,7 @@ class IRCClient:
         self.make_and_send_payload(
             command="JOINCHATROOM",
             room_name=name,
-            message="%s joined this chatroom" % (self.client_name),
+            message=f"{self.client_name} joined this chatroom",
         )
 
     def send_message(self, name, message):
@@ -119,7 +131,8 @@ class IRCClient:
 
     def send_direct_message(self, receiver, message):
         """
-        Function to send a message to another client (a DM). This will format and send the message to the server, which will relay the message to the specific receiver.
+        Function to send a message to another client (a DM). This will format and send the
+        message to the server, which will relay the message to the specific receiver.
 
         Args:
                 receiver (str): Name of the receiver, the client wishes to send a message
@@ -133,7 +146,8 @@ class IRCClient:
 
     def leave_chat_room(self, name):
         """
-        Function to leave an existing chat room. Sends the chat room that the client is part of, and server removes them from that list
+        Function to leave an existing chat room. Sends the chat room that the client is part of,
+        and server removes them from that list
 
         Args:
                 name (str): Name of the chat room from which they want to be removed
@@ -141,16 +155,17 @@ class IRCClient:
         self.make_and_send_payload(
             command="LEAVECHATROOM",
             room_name=name,
-            message="%s has left this chatroom" % (self.client_name),
+            message=f"{self.client_name} has left this chatroom",
         )
 
     def disconnect(self):
         """
-        Function to disconnect the client from the server. This sends the client a message and server kills the thread that it had assigned for the client.
+        Function to disconnect the client from the server. This sends the client a message
+        and server kills the thread that it had assigned for the client.
         """
         self.make_and_send_payload(
             command="DISCONNECT",
-            message="%s has left the server" % (self.client_name),
+            message=f"{self.client_name} has left the server",
         )
 
     def list_chat_rooms(self):
@@ -163,7 +178,8 @@ class IRCClient:
 
     def list_chat_room_clients(self, room_name):
         """
-        Function to list clients on a specific chat room. Given a chat room name, the server responds with the clients names
+        Function to list clients on a specific chat room. Given a chat room name, the server
+        responds with the clients names
 
         Args:
                 room_name (str): Name of the chat room
@@ -175,17 +191,18 @@ class IRCClient:
 
     def start_chat(self):
         """
-        Function that helps the client to communicate with the server by constantly taking inputs from the user through a while loop and sending messages back and forth.
+        Function that helps the client to communicate with the server by constantly taking
+        inputs from the user through a while loop and sending messages back and forth.
         """
-        sockets_list = [sys.stdin, self.clientSock]
+        sockets_list = [sys.stdin, self.client_sock]
 
         while True:
             try:
                 print("Input Command: ", flush=True)
-                read, write, error = select.select(sockets_list, [], [])
-                for fd in read:
-                    if fd is self.clientSock:
-                        msg = self.clientSock.recv(MSS)
+                read, _, _ = select.select(sockets_list, [], [])
+                for file_descriptor in read:
+                    if file_descriptor is self.client_sock:
+                        msg = self.client_sock.recv(MSS)
                         if not msg:
                             print(
                                 "The connection with the server has been closed",
@@ -195,7 +212,7 @@ class IRCClient:
                         else:
                             msg = msg.decode("utf-8")
                             print(msg, flush=True)
-                    elif fd is sys.stdin:
+                    elif file_descriptor is sys.stdin:
                         msg = sys.stdin.readline().replace("\n", "")
                         cmd = msg.split(" ", 1)[0]
                         if cmd == "CREATECHATROOM":
@@ -225,21 +242,23 @@ class IRCClient:
                         else:
                             print("Invalid command")
             except select.error:
-                # this happens when the socket connection has errored out, so in this case we shutdown the connection and close it.
+                # this happens when the socket connection has errored out, so
+                # in this case we shutdown the connection and close it.
                 print("closing it")
-                self.clientSock.shutdown(2)
-                self.clientSock.close()
-            except Exception as e:
+                self.client_sock.shutdown(2)
+                self.client_sock.close()
+            except Exception as err:
                 print("Exception Occurred!")
-                print(e)
+                print(err)
 
 
 def main():
     """
-    Main function to get the client name, initiate the client connection to the server and start the client to take/receieve commands.
+    Main function to get the client name, initiate the client connection to the server and
+    start the client to take/receieve commands.
     """
-    clientName = input("Enter Client Name: ").strip()
-    client = IRCClient(clientName)
+    client_name = input("Enter Client Name: ").strip()
+    client = IRCClient(client_name)
     client.start_chat()
 
 
